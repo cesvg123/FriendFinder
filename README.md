3fa22df255524598de0585369e43faeefd6598e1
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2019-11-23 10:15:28 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# GiphyAPI
+Hello...
+
+I was able to complete most of the homework except for the friend matching. I shall continue working on it the next few days hoping to update this readme.
+
+Please look at the video where you shall see the end-to-end application working. I did not spend much time working on the CSS because I wanted to get it all working and then work on the css. Hoping I could get it all working and looking nice in the next few days.
+
+Regards,
+Cesar
--- app/data/friend.js
+++ app/data/friend.js
@@ -1,130 +1,140 @@
 var friend = [
-    
+
     {
-    "name": "Cesar",
-    "Photograph": "./technics-q-c-640-480-7.png",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-    ]},
+        "name": "Cesar",
+        "Photograph": "./technics-q-c-640-480-7.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+        ]
+    },
     {
-    "name": "Nelson",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        4,
-        1,
-        5,
-        1,
-        2,
-    ]}
-    ,
+        "name": "Nelson",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            4,
+            1,
+            5,
+            1,
+            2,
+        ]
+    },
     {
-    "name": "Carmen",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        1,
-        1,
-        5,
-        1,
-        2,
-    ]},{
-    "name": "Maria",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        2,
-        2,
-        5,
-        1,
-        2,
-    ]},{
-    "name": "Karen",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        4,
-        3,
-        3,
-        1,
-        2,
-    ]},{
-    "name": "Rebecca",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        4,
-        5,
-        5,
-        1,
-        2,
-    ]},{
-    "name": "Nico",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        1,
-        4,
-        2,
-        1,
-        2,
-    ]},{
-    "name": "Nicholas",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        2,
-        4,
-        5,
-        3,
-        2,
-    ]},{
-    "name": "Pepe",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        5,
-        4,
-        4,
-        5,
-        5,
-        2,
-    ]},{
-    "name": "Teresa",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        3,
-        4,
-        5,
-        3,
-        2,
-    ]},{
-    "name": "Gerardo",
-    "Photograph": './technics-q-c-640-480-7.png',
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        4,
-        4,
-        2,
-    ]}
+        "name": "Carmen",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            1,
+            1,
+            5,
+            1,
+            2,
+        ]
+    }, {
+        "name": "Maria",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            2,
+            2,
+            5,
+            1,
+            2,
+        ]
+    }, {
+        "name": "Karen",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            3,
+            1,
+            2,
+        ]
+    }, {
+        "name": "Rebecca",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            4,
+            5,
+            5,
+            1,
+            2,
+        ]
+    }, {
+        "name": "Nico",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            1,
+            4,
+            2,
+            1,
+            2,
+        ]
+    }, {
+        "name": "Nicholas",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            2,
+            4,
+            5,
+            3,
+            2,
+        ]
+    }, {
+        "name": "Pepe",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            5,
+            5,
+            2,
+        ]
+    }, {
+        "name": "Teresa",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            3,
+            4,
+            5,
+            3,
+            2,
+        ]
+    }, {
+        "name": "Gerardo",
+        "Photograph": './technics-q-c-640-480-7.png',
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            4,
+            4,
+            2,
+        ]
+    }
 
 ];
 
