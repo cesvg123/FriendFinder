43829c02ae1aa8a2cd69963e607bbd6875ffff02
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2019-11-24 21:22:36 -0500

    update friend and survey

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,138 +1,102 @@
 var friend = [
 
     {
-        "name": "Cesar",
-        "Photograph": "./technics-q-c-640-480-7.png",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
+        name: "Cesar",
+        Photograph: "./technics-q-c-640-480-7.png",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
         ]
     },
     {
-        "name": "Nelson",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            4,
-            1,
-            5,
-            1,
-            2,
+        name: "Nelson",
+        Photograph: './technics-q-c-640-480-7.png',
+        score: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
         ]
     },
     {
-        "name": "Carmen",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            1,
-            1,
-            5,
-            1,
-            2,
+        name: "Carmen",
+        Photograph: './technics-q-c-640-480-7.png',
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
         ]
     }, {
-        "name": "Maria",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            2,
-            2,
-            5,
-            1,
-            2,
+        name: "Maria",
+        Photograph: './technics-q-c-640-480-7.png',
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
         ]
     }, {
-        "name": "Karen",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            4,
-            3,
-            3,
-            1,
-            2,
+        name: "Karen",
+        Photograph: './technics-q-c-640-480-7.png',
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
         ]
     }, {
-        "name": "Rebecca",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            4,
-            5,
-            5,
-            1,
-            2,
+        name: "Rebecca",
+        Photograph: './technics-q-c-640-480-7.png',
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
         ]
     }, {
-        "name": "Nico",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            1,
-            4,
-            2,
-            1,
-            2,
+        name: "Nico",
+        Photograph: './technics-q-c-640-480-7.png',
+        scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "5",
+            "4",
         ]
     }, {
-        "name": "Nicholas",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            2,
-            4,
-            5,
-            3,
-            2,
-        ]
-    }, {
-        "name": "Pepe",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            5,
-            4,
-            4,
-            5,
-            5,
-            2,
-        ]
-    }, {
-        "name": "Teresa",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            3,
-            4,
-            5,
-            3,
-            2,
-        ]
-    }, {
-        "name": "Gerardo",
-        "Photograph": './technics-q-c-640-480-7.png',
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            4,
-            4,
-            2,
+        name: "Gerardo",
+        Photograph: './technics-q-c-640-480-7.png',
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "4",
+            "4",
+            "2",
         ]
     }
 
